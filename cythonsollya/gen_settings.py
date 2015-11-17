4ef5e09f4e5d5d085f94e545a4a8ae0f272d74ea
Marc Mezzarobba <marc@mezzarobba.net>
2015-11-17 10:20:05 +0100

    resetting settings to default with del()

--- cythonsollya/gen_settings.py
+++ cythonsollya/gen_settings.py
@@ -27,5 +27,9 @@ for name in sollya_settings:
             return wrap(sollya_lib_get_{name}())
         def __set__(self, value):
             sollya_lib_set_{name}(as_SollyaObject(value).value)
+        def __del__(self):
+            cdef sollya_obj_t default = sollya_lib_default()
+            sollya_lib_set_{name}(default)
+            sollya_lib_clear_obj(default)
     """.format(name=name)
 print "settings = __Settings()"
