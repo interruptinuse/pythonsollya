ef3050d4f4402bc746e981d397b5f876f14f7292
Nicolas Brunie <nibrunie@gmail.com>
2017-09-18 15:16:13 +0200

    adding some explanation in examples/extra_functions test

--- examples/extra_functions.py
+++ examples/extra_functions.py
@@ -2,11 +2,17 @@
 r"""
 >>> from sollya_extra_functions import cbrt
 
+An extension module called sollya_extra_functions, provides a few functions
+which are not part of sollya library. Such as the cubic root function:
+
 >>> cbrt(1)
 1
 
 >>> cbrt(27.0) + 2.0
 5
+
+This Function accepts any input compatible with a conversion to a SollyaObject
+and returns a SolllyaObject. It can be mixed with sollya objects.
 
 >>> from sollya import log2
 >>> cbrt(log2(8.0)**3)
